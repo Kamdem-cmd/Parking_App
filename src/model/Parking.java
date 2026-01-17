f5c7bc097686f240639263b4e12464a544c0fe18
Kamdem-cmd <kamdemkevin87@gmail.com>
2026-01-17 10:58:31 +0100

    Ajout event et UI

--- src/model/Parking.java
+++ src/model/Parking.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Parking {
-    
+        private int capacitéMax;
+    private List<Place> Vehicules;
+    public Parking(int capacitéMax) {
+        this.capacitéMax = capacitéMax;
+        this.Vehicules = new ArrayList<>();
+    }
 }
--- src/model/Vehicule.java
+++ src/model/Vehicule.java
@@ -1,3 +1,23 @@
 public class Vehicule {
+    private String matricule;
+    private typeVehicule type;
+    private Ticket ticket;
     
+    public Vehicule(String matricule, typeVehicule type) {
+        this.matricule = matricule;
+        this.type = type;
+    }
+    public String getMatricule() {
+        return matricule;
+    }
+    public typeVehicule getType() {
+        return type;
+    }
+    @Override
+    public String toString() {
+        return "Vehicule{" +
+                "matricule='" + matricule + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
 }
